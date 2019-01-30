32a27f32eca2984f98bfdd021ed11d25a89bd908
visualstudio <visualstudio@122-PC28>
2019-01-30 11:23:04 -0500

    i added class countryside and launch public void

--- 730507_week 4/Program.cs
+++ 730507_week 4/Program.cs
@@ -19,9 +19,29 @@ namespace _730507_week_4
         // what KInd of DATA do we need in a NODE?
         public Village nextVillage;
         public String VillageName;
-        public bool isAstrildeHere = false;
-
-       }
+            public Village previousVillage;
+            public bool isAstrildeHere = false;
+        }
+        class countryside
+        {
+            Village maple = new Village();
+            Village Toronto = new Village();
+            Village Ajax = new Village();
+            Village First;
+            Village Last;
+                public void Launch()
+            {
+                maple.VillageName = "maple";
+                maple.nextVillage = Toronto;
+                maple.previousVillage = null;
+                Toronto.VillageName = "Toronto";
+                Toronto.nextVillage = Ajax;
+                Toronto.previousVillage = maple;
+                Ajax.VillageName = "Ajax";
+                Ajax.nextVillage = null;
+                Ajax.nextVillage = Toronto;
+            }
+        }
  
     }
 }
