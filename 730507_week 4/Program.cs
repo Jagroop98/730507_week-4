e88dbb5f702ed60d04728d1652deb35165746e5d
visualstudio <visualstudio@122-PC28>
2019-01-30 10:27:28 -0500

    village

--- 730507_week 4/Program.cs
+++ 730507_week 4/Program.cs
@@ -9,4 +9,19 @@ namespace _730507_week_4
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Node
+    {
+      class Village
+
+        {
+        // Node is an ADT
+        // what KInd of DATA do we need in a NODE?
+        public Village nextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+
+       }
+ 
+    }
 }
